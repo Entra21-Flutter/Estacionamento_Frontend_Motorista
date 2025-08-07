f415f90a6e827366728cee49f76bdbf775e21fca
Bruno Cristiano <120345236+BrunoulSn@users.noreply.github.com>
2025-08-07 11:04:17 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:vagaja/navegacao/Rotas.dart';
 import 'package:vagaja/src/app/widget_app.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(Rotas());
 
 }
